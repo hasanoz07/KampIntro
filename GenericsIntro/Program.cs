213385869a502e62f4ff9138eb24a7247d9ebbe3
Hasan ÖZ <2011404009@ogr.mehmetakif.edu.tr>
2021-02-26 23:30:32 +0300

    20.02.2021 Yazılım Geliştirici Yetiştime Kampı (Engin Demiroğ)

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Hasan");
+            Console.WriteLine(isimler.Lenght);
+            isimler.Add("Beyza");
+            Console.WriteLine(isimler.Lenght);
+            foreach (var item in isimler.Items)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
